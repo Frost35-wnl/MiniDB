69c0829ead01c099641aa0752e2d77b8da10456e
Frost35-wnl <137229836+Frost35-wnl@users.noreply.github.com>
2025-09-10 21:13:32 +0100

    Create context features
    - Create context.h for the API
    - Create context.c the core of the features
    - Test the context features (successful)

---
+++ src/context.c
@@ -0,0 +1,50 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "../include/context.h"
+
+#if !defined(PUBLIC) && !defined(PRIVATE)
+
+#define PUBLIC
+#define PRIVATE static
+
+#endif /* ifndef */ 
+
+struct context_type {
+  Table current_table;
+};
+
+PRIVATE void terminate(char *message)
+{
+  fprintf(stderr, "%s\n", message);
+  exit(EXIT_FAILURE);
+}
+
+PUBLIC DBContext init_context()
+{
+  DBContext ctx = malloc(sizeof(struct context_type));
+  if (ctx == NULL)  
+    terminate("Error in init_context : can't create a context ");
+  
+  ctx->current_table = NULL;
+
+  return ctx;
+}
+
+PUBLIC void set_current_table(DBContext ctx, Table t)
+{
+  if (ctx->current_table != NULL) {
+    free_table(ctx->current_table);
+  }
+  ctx->current_table = t;
+}
+
+PUBLIC Table get_current_table(DBContext ctx)
+{
+  return ctx->current_table;
+}
+
+PUBLIC void free_context(DBContext ctx)
+{
+  free_table(ctx->current_table);
+  free(ctx);
+}
---
+++ tests/test_context.c
@@ -0,0 +1,48 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+#include "../include/context.h"
+#include "../include/model/table_adt.h"
+#include "../include/model/row_adt.h"
+
+int main(void)
+{
+  DBContext ctx = init_context();
+  printf("The init_context function works fine\n");
+
+  const char *title1[] = {"Number", "Freq", "Total", NULL};
+  Table table1 = create_table("TABLE 1", title1);
+  const char *list1[] = {"0", "0", "0", NULL};
+  Row row1 = create_row(list1);
+  const char *list2[] = {"1", "2", "3", NULL};
+  Row row2 = create_row(list2);
+  add_row(table1, row1);
+  add_row(table1, row2);
+
+  const char *title2[] = {"True", "False", NULL};
+  Table table2 = create_table("TABLE 2", title2);
+  const char *list3[] = {"O", "X", NULL};
+  Row row3 = create_row(list3);
+  const char *list4[] = {"O", "O", NULL};
+  Row row4 = create_row(list4);
+  add_row(table2, row3);
+  add_row(table2, row4);
+
+  printf("Tables created\n");
+  
+  printf("\nSelect table1\n");
+  set_current_table(ctx, table1);
+  Table tmp = get_current_table(ctx);
+  print_table(tmp);
+
+  printf("\nSwitch table2\n");
+  set_current_table(ctx, table2);
+  Table tmp2 = get_current_table(ctx);
+  print_table(tmp2);
+
+  free_context(ctx);
+  printf("Test successfull\n");
+
+  return 0;
+  
+}
