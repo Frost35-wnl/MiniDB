08cd07482accff2970536fcd5a7c73de8cb220fd
sanabavy <137229836+sanabavy@users.noreply.github.com>
2025-09-23 23:27:22 +0100

    Update config.h
    - Remove "/" at the end of ROOT_DIR for esthetic

--- include/config.h
+++ include/config.h
@@ -2,7 +2,7 @@
 #define CONFIG_H
 
 #define DEBUG 1
-#define ROOT_DIR "../"
+#define ROOT_DIR ".." // with no "/" at the end for esthetic
 #define TABLE_NAME_WIDTH 120
 #define COLUMN_WIDTH_DISPLAY 10
 #define MAX_BUFF 500
